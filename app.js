e13adb51eee8503c3c4f55c7f6f4de9c3879f4d2
Havlc <konrad1988@interia.pl>
2022-01-17 22:12:09 +0100

    change port number

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use((req,res,next) => {
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
