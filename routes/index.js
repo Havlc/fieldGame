2fa398a283849818cfe9ed89260fde1aabbb8319
Havlc <konrad1988@interia.pl>
2022-01-24 20:29:03 +0100

    add layout for authorization and adding routes, call Task db data and pass data to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
+const Task = require('../models/Task');
 
 // Welcome page
-router.get(('/'), (req, res) => res.render('landing'));
+router.get(('/'), (req, res) => res.render('landing', {layout: 'startLayout'}));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
-);
+Task.find({}, (err, tasks)=>{
+//Task.findOne({taskNumber: "1"}, (err, task)=>{
+  if (err){
+    console.log("ERROR!");
+  } else {
+    res.render('dashboard', {
+      user: req.user, info: "informacja", tasks: tasks
+    })
+  }
+}))
 
 module.exports = router;
